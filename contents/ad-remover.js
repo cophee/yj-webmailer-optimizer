e06845c3809e88d72274601168f77e35c392f07a
mpyw <ryosuke_i_628@yahoo.co.jp>
2015-09-03 13:11:43 +0900

    Added observer

--- contents/ad-remover.js
+++ contents/ad-remover.js
@@ -1,6 +1,17 @@
 'use strict';
 
-(function (ad) {
+(function (ad, sc, observer) {
     ad.parentNode.removeChild(ad);
-    document.getElementById('shellcontent').style.right = 0;
-})(document.getElementById('theAd'));
+    sc.style.right = 0;
+    observer.observe(sc, {attributes: true});
+})(
+    document.getElementById('theAd'),
+    document.getElementById('shellcontent'),
+    new MutationObserver(function (mutations) {
+        mutations.forEach(function (record) {
+            if (record.attributeName === 'style') {
+                record.target.style.right = 0;
+            }
+        });
+    })
+);
